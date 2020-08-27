98f07a285713afb83778569b6762e8fa648ae50e
oov <ilovevipper@gmail.com>
2020-08-28 01:01:59 +0900

    bump version

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
 @echo off
 cd bin
-..\7za.exe a -tzip ..\forcepser.zip forcepser.exe forcepser.txt _entrypoint.lua template.exo-template setting.txt-template asas
+..\7za.exe a -tzip ..\forcepser.zip forcepser.exe forcepser.txt _entrypoint.lua setting.txt-template asas
 cd ..
