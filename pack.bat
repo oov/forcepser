698a4076b6e39d16eee6f2bb6e7c26a6a7663dcf
oov <ilovevipper@gmail.com>
2021-03-03 07:49:49 +0900

    remove forcepser_config.exe from archive

--- pack.bat
+++ pack.bat
@@ -1,6 +1,4 @@
 @echo off
-copy ..\forcepser_config_exe\Win32_Release\forcepser_config.exe .\bin\
-copy ..\forcepser_config_exe\Win32_Release\WebView2Loader.dll .\bin\
 cd bin
-..\7za.exe a -tzip ..\forcepser.zip forcepser.exe forcepser_config.exe WebView2Loader.dll forcepser.txt _entrypoint.lua setting.txt-template setting.txt-template-old asas
+..\7za.exe a -tzip ..\forcepser.zip forcepser.exe forcepser.txt _entrypoint.lua setting.txt-template setting.txt-template-old asas
 cd ..
