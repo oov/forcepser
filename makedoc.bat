68ccd186804ea988f66ba252fa6dd4ea6374fab4
oov <ilovevipper@gmail.com>
2022-03-17 05:14:35 +0900

    add releaser and cleanup dir

--- makedoc.bat
+++
@@ -1,7 +0,0 @@
-git rm -rf docs/*
-mkdir docs
-cd ../forcepser_config_html
-call pack.bat
-cp -r build/* ../forcepser/docs/
-cd ../forcepser
-git add docs
--- pack.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd bin
-..\7za.exe a -tzip ..\forcepser.zip forcepser.exe forcepser.txt _entrypoint.lua setting.txt-template setting.txt-template-old asas
-cd ..
